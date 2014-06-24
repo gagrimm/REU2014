ddd04adc71ff69420df1bfd531e908184894d3f5
zdc9pd <zdc9pd@mail.missouri.edu>
2014-06-24 15:02:27 -0500

    video update

--- video player buttons/js/global.js
+++ video player buttons/js/global.js
@@ -2,7 +2,7 @@ var i = 0;
 var videolist = [
 	"http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" ,
 	"http://video-js.zencoder.com/oceans-clip.mp4" ,
-	"//www.youtube.com/embed/oQssF0v8_I4"
+	"http://pdl.vimeocdn.com/89496/595/203684545.mp4?token2=1403640971_65e53a5e640f4cbea6602238e27748c3&aksessionid=3f582da4f519ea51"
 ];
 var x = document.getElementById("example_video_1");
 
